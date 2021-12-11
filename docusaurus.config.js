937c4ab5f3666bb8e9566100345d72b376985952
오종택 <jongtaek@ojongtaeg-ui-MacBookPro.local>
2021-12-12 02:50:00 +0900

    Add: 1장 - 리팩터링: 첫 번째 예시

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,32 +1,35 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (
   module.exports = {
-    title: 'Refactoring 2.0',
-    tagline: '리팩터링2.0 완독하기 프로젝트',
-    url: 'https://..com',
-    baseUrl: '/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    organizationName: 'dev-in-book', // Usually your GitHub org/user name.
-    projectName: 'refactoring', // Usually your repo name.
+    title: "Refactoring 2.0",
+    tagline: "리팩터링2.0 완독하기 프로젝트",
+    url: "https://..com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "dev-in-book", // Usually your GitHub org/user name.
+    projectName: "refactoring", // Usually your repo name.
 
     presets: [
       [
-        '@docusaurus/preset-classic',
+        "@docusaurus/preset-classic",
         /** @type {import('@docusaurus/preset-classic').Options} */
         ({
           docs: {
-            sidebarPath: require.resolve('./sidebars.js'),
-            editUrl: 'https://github.com/dev-in-book/refactoring',
+            sidebarPath: require.resolve("./sidebars.js"),
+            editUrl: "https://github.com/dev-in-book/refactoring",
           },
           blog: {
             showReadingTime: true,
-            editUrl: 'https://github.com/dev-in-book/refactoring',
+            editUrl: "https://github.com/dev-in-book/refactoring",
+          },
+          theme: {
+            customCss: [require.resolve("./src/css/custom.css")],
           },
         }),
       ],
@@ -36,43 +39,43 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
       /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
         navbar: {
-          title: '리팩터링 2.0',
+          title: "리팩터링 2.0",
           logo: {
-            alt: 'dev-in-book',
-            src: 'img/refactoring.jpg',
+            alt: "dev-in-book",
+            src: "img/refactoring.jpg",
           },
           items: [
             {
-              type: 'doc',
-              docId: 'chapter_1',
-              position: 'left',
-              label: '📕 리팩터링 완독하기 프로젝트',
+              type: "doc",
+              docId: "chapter_1",
+              position: "left",
+              label: "📕 리팩터링 완독하기 프로젝트",
             },
-            { to: '/blog', label: '👨‍👩‍👧‍👦 참여자', position: 'left' },
+            { to: "/blog", label: "👨‍👩‍👧‍👦 참여자", position: "left" },
             {
-              href: 'https://github.com/dev-in-book/refactoring',
-              label: '⛳️ GitHub',
-              position: 'right',
+              href: "https://github.com/dev-in-book/refactoring",
+              label: "⛳️ GitHub",
+              position: "right",
             },
           ],
         },
         footer: {
-          style: 'dark',
+          style: "dark",
           links: [
             {
-              title: 'Docs',
+              title: "Docs",
               items: [
                 {
-                  label: 'Refactoring 2.0',
-                  to: '/docs/chapter_1',
+                  label: "Refactoring 2.0",
+                  to: "/docs/chapter_1",
                 },
                 {
-                  label: 'Member',
-                  href: '/blog/member',
+                  label: "Member",
+                  href: "/blog/member",
                 },
                 {
-                  label: 'GitHub',
-                  href: 'https://github.com/dev-in-book/refactoring',
+                  label: "GitHub",
+                  href: "https://github.com/dev-in-book/refactoring",
                 },
               ],
             },
