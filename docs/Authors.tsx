f744d5e22562c8e01e9990374d23060b3d238f1c
saengmotmi <ohjtack@gmail.com>
2022-02-05 22:13:20 +0900

    add: 10-2

--- docs/Authors.tsx
+++ docs/Authors.tsx
@@ -1,40 +1,40 @@
-import React, { useCallback } from 'react';
-import styles from './Authors.module.css';
+import React, { useCallback } from "react";
+import styles from "./Authors.module.css";
 
 const AUTHORS = {
   positiveko: {
-    name: 'positiveko',
-    title: 'Front End Engineer',
-    url: 'https://github.com/positiveko',
-    image_url: 'https://github.com/positiveko.png',
+    name: "positiveko",
+    title: "Front End Engineer",
+    url: "https://github.com/positiveko",
+    image_url: "https://github.com/positiveko.png",
   },
   saengmotmi: {
-    name: 'saengmotmi',
-    title: 'Front End Engineer',
-    url: 'https://github.com/saengmotmi',
-    image_url: 'https://github.com/saengmotmi.png',
+    name: "saengmotmi",
+    title: "Front End Engineer",
+    url: "https://github.com/saengmotmi",
+    image_url: "https://github.com/saengmotmi.png",
   },
-  'Woongjo-Yoo': {
-    name: 'Woongjo-Yoo',
-    title: 'Back End Engineer',
-    url: 'https://github.com/onikss793',
-    image_url: 'https://github.com/onikss793.png',
+  "Woongjo-Yoo": {
+    name: "Woongjo-Yoo",
+    title: "Back End Engineer",
+    url: "https://github.com/onikss793",
+    image_url: "https://github.com/onikss793.png",
   },
   Mengkki: {
-    name: 'Mengkki',
-    title: 'Front End Engineer',
-    url: 'https://github.com/guswnl0610',
-    image_url: 'https://github.com/guswnl0610.png',
+    name: "Mengkki",
+    title: "Front End Engineer",
+    url: "https://github.com/guswnl0610",
+    image_url: "https://github.com/guswnl0610.png",
   },
   Danbi: {
-    name: 'Danbi',
-    title: 'Front End Engineer',
-    url: 'https://github.com/itssweetrain',
-    image_url: 'https://github.com/itssweetrain.png',
+    name: "Danbi",
+    title: "Android Engineer",
+    url: "https://github.com/itssweetrain",
+    image_url: "https://github.com/itssweetrain.png",
   },
-};
+} as const;
 
-type TAuthor = 'positiveko' | 'saengmotmi' | 'Woongjo-Yoo' | 'Mengkki | Danbi';
+type TAuthor = keyof typeof AUTHORS;
 
 interface IAuthors {
   bookLeader: TAuthor;
@@ -52,9 +52,10 @@ export default function Authors({
       <div className={styles.author}>
         <a
           href={authorInfo.url}
-          target='_blank'
-          rel='noopener noreferrer'
-          className={styles.avatar__link}>
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.avatar__link}
+        >
           <img
             src={authorInfo.image_url}
             alt={author}
